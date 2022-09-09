69f61527a988b4a05a9382453913949be7a5ee00
uche from termux <uunigwe74@gmail.com>
2022-09-09 11:28:47 -0700

    meat

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,15 +17,15 @@ int main(void)
 	/* your code goes there */
 	if (n == 0)
 	{
-		puts("%d is zero", n);
+		printf("%d is zero", n);
 	}
 	else if (n > 0)
 	{
-		puts("%d is positive", n);
+		printf("%d is positive", n);
 	}
 	else
 	{
-		puts("%d is negative", n);
+		printf("%d is negative", n);
 	}
 	return (0);
 }
