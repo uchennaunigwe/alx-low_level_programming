7bd848cba3c729882895c4c9e5de0c7a01653ffa
uche from termux <uunigwe74@gmail.com>
2022-09-09 18:53:53 -0700

    meat

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Return: Always return 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	ch = 0;
+	do {
+		putchar (ch + '0');
+		ch++;
+	} while (ch <= 9);
+	putchar ('\n');
+	return (0);
+}
