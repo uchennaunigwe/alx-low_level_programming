ca56d7ada34b393d4fccfd12476186d9dcf25832
uche from termux <uunigwe74@gmail.com>
2022-09-13 23:35:06 -0700

    meat

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include "main.h"
+
+#endif
+/**
+ * print_last_digit - Entrypoint
+ * @n: Evaluated value
+ *
+ * Return: On sucess 1.
+ * On failure, -1
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
