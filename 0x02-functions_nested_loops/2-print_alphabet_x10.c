48b0d55809122e7ccccfd2b55a6ea711f208b6f6
uche from termux <uunigwe74@gmail.com>
2022-09-13 20:35:46 -0700

    meat

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include "main.h"
+
+#endif
+/**
+ * print_alphabet - Entrypoint
+ * Return: Always 0 (sucess)
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		char i;
+
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
