894d9078eba08222809802ac9024b1349ad96b98
uche from termux <uunigwe74@gmail.com>
2022-09-13 23:28:33 -0700

    meat

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include <stdlib.h>
+#include "main.h"
+
+#endif
+/**
+ * _abs - Entrypoint
+ * @n: Evaluated value
+ *
+ * Return: On sucess 1.
+ * On failure, -1
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
