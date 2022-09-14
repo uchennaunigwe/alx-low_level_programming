b0ea7a85b7002fd1914568445b785648671c2ac9
uche from termux <uunigwe74@gmail.com>
2022-09-13 21:06:47 -0700

    meat

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <ctype.h>
+#include "main.h"
+
+#endif
+/**
+ * _islower - Entrypoint
+ * Return: Always 0 (sucess)
+ */
+int _islower(int c)
+{
+	return islower(c);
+}
