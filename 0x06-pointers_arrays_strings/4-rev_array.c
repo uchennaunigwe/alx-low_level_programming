41c2116dce225f4de561fa2e6c553feeaef7e64c
uche from termux <uunigwe74@gmail.com>
2022-09-21 18:51:12 -0700

    me

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * reverse_array - Join pointers
+ * @a: dest
+ * @n: src
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, temp;
+
+	for (i = 0; i < n/2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
+	}
+}
