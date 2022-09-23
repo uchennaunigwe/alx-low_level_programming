1bbab7d8ad5cb276e36e52a627cf46bf9bd30502
uche from termux <uunigwe74@gmail.com>
2022-09-23 12:18:09 -0700

    me

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strlen - swap
+ * @s: string
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int temp;
+
+	temp = strlen(s);
+	return (temp);
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _puts - print  string
+ * @c: string to print
+ */
+
+void _puts(char *c)
+{
+	puts (c);
+}
