2eaaf414f8cfe9b233ec7f81f713dacf8aec4e5c
uche from termux <uunigwe74@gmail.com>
2022-09-27 12:45:35 -0700

    me

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * print_diagsums - Join pointers
+ * @a: string
+ * @size: search character
+ * Return: joined string
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
