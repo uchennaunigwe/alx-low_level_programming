e247f5d7f0b7bf6fbfd949f2e2fa7a31324dec48
uche from termux <uunigwe74@gmail.com>
2022-09-27 11:59:12 -0700

    me

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include "main.h"
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * puts_half - print  string
+ * @a: array of int to print
+ * @n: number of element to print
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n ; i++)
+	{
+		printf ("%d", *(a + i));
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf ("\n");
+}
