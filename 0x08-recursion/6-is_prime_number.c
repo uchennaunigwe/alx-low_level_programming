fc4141f87bfc498070d39658410157dc101de4d2
uche from termux <uunigwe74@gmail.com>
2022-09-28 14:02:13 -0700

    me

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _pow_recursion - Entry
+ * @n: operand
+ * Return: Me
+ */
+
+int is_prime_number(int n)
+{
+	if (n % 2 == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
