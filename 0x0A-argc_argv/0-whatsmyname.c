88ea253c2c34df6940abd9269399ebacb0850578
uche from termux <uunigwe74@gmail.com>
2022-10-05 21:53:39 -0700

    me

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * main - Entry
+ * @argc: operand
+ * @argv: power
+ * Return: Me
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
