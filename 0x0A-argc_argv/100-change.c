c8d9e3e66af484a30adc46e47fa703f20cdf2fe1
uche from termux <uunigwe74@gmail.com>
2022-10-17 11:54:07 -0700

    me

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * main - Entry
+ * @argc: operand
+ * @argv: power
+ * Return: Me
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, mul;
+
+	mul = 0;
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]) >= 0)
+			{
+				mul = mul + atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	printf("%d\n", mul);
+	return (0);
+}
